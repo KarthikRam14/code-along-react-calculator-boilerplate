6fac99369373bb05b9a388d510d918210e52aa92
KarthikRam14 <karthikram1441@gmail.com>
2024-02-24 00:24:31 +0530

    updated

---
+++ calculator/src/components/Keys.jsx
@@ -0,0 +1,15 @@
+import { ACTIONS } from "../App"
+
+function Operations({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
+
+export default Operations;
